076f7a501ec9f486909287c2e48a3035be70df8e
Phoenix3x3 <andrey.bernatski2000@gmail.com>
2020-05-08 17:10:57 +0300

    add to the prod

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI:
+    "mongodb://heroku_z7k4xn98:fv3rqqngof5bii6gqaihmsnlg6@ds263638.mlab.com:63638/heroku_z7k4xn98",
+};
--- server/server.js
+++ server/server.js
@@ -27,4 +27,12 @@ app.use("/api/authorizate", authorizateRouter);
 //
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
